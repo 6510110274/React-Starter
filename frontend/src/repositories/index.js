c0507b5f10bcc25751df50e42a0f77d7527a65f8
Papanya Siriwattanaworakul <papanya2546@gmail.com>
2025-04-03 16:25:56 +0700

    feat: add OrderRepository to repositories for order management

--- frontend/src/repositories/index.js
+++ frontend/src/repositories/index.js
@@ -1,7 +1,9 @@
 import { UserRepository } from './UserRepository.js'; 
+import { OrderRepository } from './OrderRepository.js';
 
 const repositories = {
-    users : new UserRepository()
+    users : new UserRepository(),
+    oders : new OrderRepository()
 }
 
 export default repositories; // Export the repositories object as the default export
